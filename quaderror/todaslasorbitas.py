d42ef71486d1b42730514162be969b2f110d383e
Andrea Velasquez <andreaaa21@hotmail.com>
2017-11-09 20:15:57 -0500

    bueno, se hizo lo que se pudo

---
+++ quaderror/todaslasorbitas.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+import os
+import sys
+output="todaslasorbitas.gplot"
+pf = open(output,'w')
+#inicial = "  gnuplot -p -e \"p  "
+graphtype = "set term postscript eps\n"
+pf.write(graphtype)
+graphname = "set output \"plot.eps\"\n"
+pf.write(graphname)
+inicial = " p "
+#print >>pf, inicial
+pf.write(inicial)
+for i in range(1,300):
+    cmdsystem = "./readtrackone1.py " + "1 " + "1" + " 300 "+ str(i)+ " BPM.29R1.B1"
+    print cmdsystem
+    os.system(cmdsystem)
+    cmdsystem = "cp orbita.dat orbita" + str(i)+".dat"
+    print cmdsystem
+    os.system(cmdsystem)
+    cmdsystem = "\'./orbita"+str(i)+".dat\' u 1:2 w l notitle, "
+    #print >> pf, cmdsystem
+    pf.write(cmdsystem)
+cmdsystem = "./readtrackone1.py " + "1 " + "1" + " 300 "+ str(i)+ " BPM.29R1.B1"
+print cmdsystem
+os.system(cmdsystem)
+cmdsystem = "cp orbita.dat orbita" + str(i+1)+".dat"
+print cmdsystem
+os.system(cmdsystem)
+cmdsystem = "\'./orbita"+str(i+1)+".dat\' u 1:2 w l notitle, \'./orbita"+str(i+1)+".dat\' u 1:3 w l notitle  "
+#print >> pf, cmdsystem
+pf.write(cmdsystem)
